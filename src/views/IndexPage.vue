f572a6a90cbd9bff159ffb567725d7082843ec04
shohrux.burxonov <shohrukh.burkhonov@alif.uz>
2024-03-16 03:13:37 +0500

    INITIAL_COMMIT

---
+++ src/views/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <IndexTable />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import IndexTable from '../components/IndexTable.vue';
+</script>
+
+<style lang="scss" scoped></style>
